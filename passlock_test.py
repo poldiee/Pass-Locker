a43b2ad264d8f01cbaa60deec9e62f237648dc9b
poldiee <nessidave@gmail.com>
2022-04-28 16:58:18 +0300

    test class user credentials

---
+++ passlock_test.py
@@ -0,0 +1,22 @@
+import unittest
+from passlock import User
+
+class UserTest(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('poldiee','XyZ3thf1')
+
+    def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'poldiee')
+        self.assertEqual(self.new_user.password,'XyZ3thf1')
+
+if __name__ == "__main__":
+    unittest.main()
