5589ce64e89adfd14f6e9991e561cbafdef44716
poldiee <nessidave@gmail.com>
2022-04-28 17:46:50 +0300

    user interaction with app file

--- passlock.py
+++ passlock.py
@@ -88,4 +88,10 @@ class Credentials():
     def generatePassword(self,stringLength=8):
         """Generate a random password string of letters and digits and special characters"""
         password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        
+        
+        
         return ''.join(random.choice(password) for i in range(stringLength))
+
+
+    
