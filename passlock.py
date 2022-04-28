5893c58919d6356b174bee1b59c3854f538cea63
poldiee <nessidave@gmail.com>
2022-04-28 17:03:30 +0300

    saving user method test

--- passlock.py
+++ passlock.py
@@ -2,8 +2,8 @@ class User:
     """
     Create User class that generates new instances of a user.
     """
-    
-    user = []
+
+    user_list = []
 
     def __init__(self, username, password):
         """
@@ -11,3 +11,9 @@ class User:
         """
         self.username = username
         self.password = password 
+
+    def save_user(self):
+        """
+        A method that saves a new user instace into the user list
+        """
+        User.user_list.append(self)
