c76da7ef841627a21f57214fb3a47d86b4b7600f
poldiee <nessidave@gmail.com>
2022-04-28 18:28:52 +0300

    interface logins

--- interface.py
+++ interface.py
@@ -35,23 +35,24 @@ def login_user(username,password):
   check_user = Credentials.verify_user(username,password)
   return check_user
 def passlocker():
-    print("Hello Welcome to your Accounts credentials store...\n 1.  Create New Account ----- CA \n 2.  Have An Account -------- LI \n 3.  Close ------------------ EX")
-    short_code=input("").lower.strip()
+    print("Hello Welcome to your Accounts credentials place...\n 1.  Create New Account ----- CA \n 2.  Have An Account -------- LI \n")
+    short_code=input("").lower().strip()
     if short_code == "ex":
-        print("It was nice having you. Welcome again.......bye!")
-    elif short_code == "ca":
+    #     print("nice having you. Welcome again.......bye!")
+    # elif short_code == "ca":
         print("Sign Up")
         print('*' * 50)
         username = input("User_name: ")
         password = input("Password: ")
         save_user(create_new_user(username,password))
-        print(f"Hello {username}, account created succesfully!")
+        
         print("*"*50)
+        print(f"Hello {username}, account created succesfully!")
         print('\n')
     elif short_code == "li":
-        print("Login! ")
+        # print("Login! ")
         print("*"*50)
-        print("Enter your User name and your Password to log in:")
+        print("Enter your User name & Password to log in:")
         print('*' * 50)
         username = input("User name: ")
         password = input("password: ")
@@ -64,6 +65,8 @@ def passlocker():
     while True:
         print("Use these short codes : cc - Create a new credential, dc - Display Credential(s), fc - Find a credential,ex - Exit the application, gp- Generate A randomn password, del- Delete credential,cp-Copy Password")
         short_code = input().lower()
+    else:
+        print("Please enter a valid input 2 continue")
         
 if __name__ == '__main__':
     passlocker() 
